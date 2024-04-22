a47906819e8324e0c2a8301bf399371a11e9594e
Paolo Gomez <peweegardner248@gmail.com>
2024-04-21 19:44:25 -0700

    update

--- step1.js
+++ step1.js
@@ -1,26 +1,13 @@
 const fs = require('fs');
-const { exit } = require('process');
 
-// Function to read file contents
 function cat(path) {
   fs.readFile(path, 'utf8', (err, data) => {
     if (err) {
-      console.error(`Error reading ${path}:`);
-      console.error(err);
-      exit(1);
+      console.error(`Error reading ${path}: ${err}`);
+      process.exit(1);
     }
     console.log(data);
   });
 }
 
-// Main function
-function main() {
-  const args = process.argv.slice(2);
-  if (args.length !== 1) {
-    console.error('Usage: node step1.js <filename>');
-    exit(1);
-  }
-  cat(args[0]);
-}
-
-main();
+cat(process.argv[2]);
--- step3.js
+++ step3.js
@@ -1,99 +1,54 @@
 const fs = require('fs');
 const axios = require('axios');
-const { exit } = require('process');
 
-// Function to read file contents
-function cat(path) {
-  fs.readFile(path, 'utf8', (err, data) => {
-    if (err) {
-      console.error(`Error reading ${path}:`);
-      console.error(err);
-      exit(1);
-    }
-    console.log(data);
-  });
-}
-
-// Function to write file contents
-function catWrite(path, filename) {
-  fs.readFile(path, 'utf8', (err, data) => {
-    if (err) {
-      console.error(`Error reading ${path}:`);
-      console.error(err);
-      exit(1);
-    }
-    fs.writeFile(filename, data, (err) => {
+function handleOutput(text, out) {
+  if (out) {
+    fs.writeFile(out, text, 'utf8', err => {
       if (err) {
-        console.error(`Couldn't write ${filename}:`);
-        console.error(err);
-        exit(1);
+        console.error(`Couldn't write ${out}: ${err}`);
+        process.exit(1);
       }
     });
-  });
-}
-
-// Function to read URL contents
-async function webCat(url) {
-  try {
-    const response = await axios.get(url);
-    console.log(response.data);
-  } catch (error) {
-    console.error(`Error fetching ${url}:`);
-    console.error(error);
-    exit(1);
-  }
-}
-
-// Function to write URL contents
-async function webCatWrite(url, filename) {
-  try {
-    const response = await axios.get(url);
-    fs.writeFile(filename, response.data, (err) => {
-      if (err) {
-        console.error(`Couldn't write ${filename}:`);
-        console.error(err);
-        exit(1);
-      }
-    });
-  } catch (error) {
-    console.error(`Error fetching ${url}:`);
-    console.error(error);
-    exit(1);
-  }
-}
-
-// Main function
-function main() {
-  const args = process.argv.slice(2);
-  const outIndex = args.indexOf('--out');
-  
-  if (outIndex === -1) {
-    if (args.length !== 1) {
-      console.error('Usage: node step3.js [--out filename.txt] <filename or URL>');
-      exit(1);
-    }
-    const arg = args[0];
-    if (arg.startsWith('http')) {
-      webCat(arg);
-    } else {
-      cat(arg);
-    }
   } else {
-    if (outIndex === 0 || outIndex === args.length - 1) {
-      console.error('Usage: node step3.js [--out filename.txt] <filename or URL>');
-      exit(1);
-    }
-    const filename = args[outIndex + 1];
-    const paths = outIndex === 0 ? args.slice(2) : args.slice(0, outIndex);
-
-    paths.forEach(path => {
-      if (path.startsWith('http')) {
-        webCatWrite(path, filename);
-      } else {
-        catWrite(path, filename);
-      }
-    });
+    console.log(text);
   }
 }
 
-main();
+function cat(path, out) {
+  fs.readFile(path, 'utf8', (err, data) => {
+    if (err) {
+      console.error(`Error reading ${path}: ${err}`);
+      process.exit(1);
+    }
+    handleOutput(data, out);
+  });
+}
+
+async function webCat(url, out) {
+  try {
+    const response = await axios.get(url);
+    handleOutput(response.data, out);
+  } catch (error) {
+    console.error(`Error fetching ${url}: ${error}`);
+    process.exit(1);
+  }
+}
+
+let path;
+let out;
+
+const argv = process.argv.slice(2);
+const outIndex = argv.indexOf('--out');
+
+if (outIndex !== -1) {
+  out = argv[outIndex + 1];
+  path = argv[outIndex + 2];
+} else {
+  path = argv[0];
+}
+
+if (path.startsWith('http')) {
+  webCat(path, out);
+} else {
+  cat(path, out);
+}
